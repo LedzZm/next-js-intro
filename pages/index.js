7756143f6e2ac223cded9aae71ecaf67c3e253af
Marios Anagnostopoulos <mariosranag@gmail.com>
2021-04-13 20:09:45 +0300

    Incorporates dynamic routing.

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,40 @@
-import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
 
-export default function Home() {
-  return (
-      <Layout home>
-        <Head>
-          <title>{siteTitle}</title>
-        </Head>
-        <section className={utilStyles.headingMd}>
-          <p>Hello, I'm Marios. I'm a software engineer. You can contact me on {' '}
-              <a href="https://www.linkedin.com/in/mranag/">Linked In</a>
-          </p>
-          <p>This is the first attempt in creating a NextJs Website.</p>
-        </section>
-      </Layout>
-  )
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData()
+    return {
+        props: {
+            allPostsData
+        }
+    }
+}
+
+export default function Home({ allPostsData }) {
+    return (
+        <Layout home>
+            {/* Keep the existing code here */}
+
+            {/* Add this <section> tag below the existing <section> tag */}
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                <h2 className={utilStyles.headingLg}>Blog</h2>
+                <ul className={utilStyles.list}>
+                    {allPostsData.map(({ id, date, title }) => (
+                        <li className={utilStyles.listItem} key={id}>
+                            <Link href={`/posts/${id}`}>
+                                <a>{title}</a>
+                            </Link>
+                            <br />
+                            <small className={utilStyles.lightText}>
+                                <Date dateString={date} />
+                            </small>
+                        </li>
+                    ))}
+                </ul>
+            </section>
+        </Layout>
+    )
 }
